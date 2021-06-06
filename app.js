287910d94e811f2ef83d6555b254c805f89df226
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-06-05 21:28:04 -0500

    setup custom routers

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 // instantiates express
 const app = express();
 
+// setup morgan Middleware
+// adds useful route response data in console
+app.use(morgan('dev'));
 // to tell express to expect JSON
 app.use(express.json());
+
+// 1) Middleware
 
 // middleware
 // location matters, acts on all routes in current location
@@ -28,6 +34,8 @@ const tours = JSON.parse(
     `${__dirname}/dev-data/data/tours-simple.json`
   )
 );
+
+// 2) Route Handlers
 
 // all functions written below were route handlers
 // these will be moved to other files later
@@ -70,7 +78,10 @@ const createTour = (req, res) => {
   // console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.assign(
+    { id: newId },
+    req.body
+  );
 
   tours.push(newTour);
   fs.writeFile(
@@ -117,27 +128,87 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not implemented',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not implemented',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not implemented',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not implemented',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not implemented',
+  });
+};
+
 // app.get('/api/v1/tours', getAllTours);
 // app.post('/api/v1/tours', createTour);
 // app.get('/api/v1/tours/:id', getTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
+// custom routers
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
 // refactored routes to reduced route strings and simplify code
-app
-  .route('/api/v1/tours')
+tourRouter
+  .route('/')
   .get(getAllTours)
   .post(createTour);
 
-app
-  .route('/api/v1/tours/:id')
+tourRouter
+  .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
+userRouter
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+userRouter
+  .route('/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// using custom routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// 4) Start Server
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`App is running on port ${port}...`);
+  console.log(
+    `App is running on port ${port}...`
+  );
 });
 
 // below was above, just used for exploring basic express concepts
