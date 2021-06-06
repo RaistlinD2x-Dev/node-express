5f7f19b697a25d131f022bb6858ab0204f96db84
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-06-06 00:02:47 -0500

    reverted NODE_ENV, using windows things don't work the same

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+const app = require('./app');
 
-const port = 3000;
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(
     `App is running on port ${port}...`
