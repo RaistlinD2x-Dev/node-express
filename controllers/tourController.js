8b893623ab54c846ecea75e02af4c63596da76fe
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-06-05 21:45:43 -0500

    sweet refactor

---
+++ controllers/tourController.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+
+// top level code to load all data from the file in a synchronous
+// fashion so it is always available globally
+// also parsed file input as JSON data
+const tours = JSON.parse(
+  fs.readFileSync(
+    `${__dirname}/../dev-data/data/tours-simple.json`
+  )
+);
+
+// 2) Route Handlers
+
+// all functions written below were route handlers
+// these will be moved to other files later
+exports.getAllTours = (req, res) => {
+  // tested req.requestTime and added to envelope here
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'Success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  //   console.log(req.params);
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  //   if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(
+    { id: newId },
+    req.body
+  );
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (parseInt(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (parseInt(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
